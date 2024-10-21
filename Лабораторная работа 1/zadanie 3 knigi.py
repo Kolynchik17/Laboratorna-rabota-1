320a0ebd0a2d4dc76289ca8cefc6ddb121ad06aa
Kolynchik17 <nikolajmuhametzanov710@gmail.com>
2024-10-21 23:01:56 +0300

    Add files via upload

---
+++ Лабораторная работа 1/zadanie 3 knigi.py
@@ -0,0 +1,9 @@
+# TODO Найдите количество книг, которое можно разместить на дискете
+obiom=(2**20)*1.44
+stranic=100
+strok=50
+simvstrok=25
+obsim=4
+kniga=obsim*simvstrok*strok*stranic
+vsego=obiom//kniga
+print("Количество книг, помещающихся на дискету:", vsego)
